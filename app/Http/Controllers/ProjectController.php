9a15d150e86a401e22c102271bce8b0e42acb249
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-07-21 08:06:02 +0100

    change storage path

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -37,7 +37,7 @@ class ProjectController extends Controller
 
 
 
-         $imgPath = $request->projectImg->store('projectImgs');
+         $imgPath = $request->projectImg->store('public/projectImgs');
         Projects::create([
             'title' => $request->projectTitle,
             'description' => $request->projectDescription,
