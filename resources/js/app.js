44107386eea4ef6660699643a5f861921e83ac70
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-11-29 21:05:39 +0100

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 const appName = import.meta.env.VITE_APP_NAME || 'Dia';
 
 createInertiaApp({
-    title: (title) => `${title} - Dia`,
+    title: (title) => `${title}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
