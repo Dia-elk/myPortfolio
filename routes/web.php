7380f44226a7032e70f5136cac045415fda41694
Dia <macbook@MacBook-Pro-de-macbook.local>
2023-12-05 19:46:51 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,8 @@
 <?php
 
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\ContactMeController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\ProjectController;
-use App\Models\projects;
-use Illuminate\Foundation\Application;
+
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,22 +15,7 @@ use Inertia\Inertia;
 |
 */
 
-Route::middleware('auth')->group(function (){
-    Route::resource('dashboard',DashboardController::class);
-    Route::resource('projects',ProjectController::class);
-    Route::resource('contactMe',ContactMeController::class);
+Route::get('/',function (){
+    return "Dia Portfolio";
 });
-
-Route::get('login', function ()
-{
-    return Inertia::render('Login');
-})->name('login');
-
-Route::post('login',[\App\Http\Controllers\LoginController::class,'store'])->name('login.store');
-
-Route::post('register',[\App\Http\Controllers\RegisterController::class,'store'])->name('register.store');
-Route::get('scabandri',[\App\Http\Controllers\RegisterController::class,'index']);
-
-Route::delete('logout',[\App\Http\Controllers\LoginController::class , 'destroy'])->name('logout');
-
 
